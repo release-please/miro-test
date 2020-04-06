5a2b8e7746218524e52b6613fe56ced344d34ad0
Dmitriy <qxwwxp@gmail.com>
2020-04-07 01:48:05 +0500

    build(webpack): Добавить `source map`

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,38 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/app/index.ts",
+  entry: './src/app/index.ts',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/public/index.html",
+      filename: 'index.html',
+      template: './src/public/index.html',
     }),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, "src/public"),
+    contentBase: path.join(__dirname, 'src/public'),
     compress: true,
     port: 9000,
   },
-};
+  devtool: 'inline-source-map'
+}
