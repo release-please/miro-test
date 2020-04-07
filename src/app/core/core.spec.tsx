c93e45f4cc82658225d941fc5b465cfcd7f69af3
Dmitriy <qxwwxp@gmail.com>
2020-04-07 23:43:04 +0500

    feat(core): Implement `render` function
    + test suites

--- src/app/core/core.spec.tsx
+++ src/app/core/core.spec.tsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h } from '.'
+import * as Core from '.'
 
 // GivenWhenThen
 // HACK: Я тут что-то напрогал для лучшего чтения тестов.
 // Пока что это работает.
-let ctx = {}
+const ctx = {}
 function given (labelOrFn: string | Function, fn?: Function): void {
   if (typeof labelOrFn === 'string') {
     fn?.(ctx)
@@ -26,90 +27,285 @@ function then (labelOrFn: string | Function, fn?: Function): void {
     labelOrFn(ctx)
   }
 }
-function clean (): void {
-  ctx = {}
-}
 
-describe('Test `h` - function', () => {
-  beforeEach(() => {
-    clean()
+describe('Core test', () => {
+  it.todo('Refactor: DRY')
+  it.todo('Research: Given When Then - pattern')
+  it.todo('Research: `Core.render()` prop handlers. Patterns, loop, if-else alternative. See TODO')
+
+  describe('`h` - function', () => {
+    it('Should return vnode with `ref` attribute', () => {
+      let actualVNode: vNode
+      let refName: string
+      let expectedVNode: vNode
+
+      given(() => {
+        refName = 'test-ref'
+
+        expectedVNode = {
+          tag: 'div',
+          props: {
+            ref: refName,
+          },
+          child: [],
+        }
+      })
+
+      when(() => {
+        actualVNode = <div ref={refName}></div>
+      })
+
+      then(() => {
+        expect(actualVNode).toStrictEqual(expectedVNode)
+      })
+    })
+
+    it('Should return vnode with empty `attr`', () => {
+      let actualVNode: vNode
+      let expectedVNode: vNode
+
+      given(() => {
+        expectedVNode = {
+          tag: 'div',
+          props: null,
+          child: [],
+        }
+      })
+
+      when(() => {
+        actualVNode = <div></div>
+      })
+
+      then(() => {
+        expect(actualVNode).toStrictEqual(expectedVNode)
+      })
+    })
+
+    it('Should return vnode with nested child', () => {
+      let actualVNode: vNode
+      let expectedVNode: vNode
+
+      given(() => {
+        expectedVNode = {
+          tag: 'div',
+          props: null,
+          child: [
+            {
+              tag: 'h1',
+              props: null,
+              child: [],
+            },
+          ],
+        }
+      })
+
+      when(() => {
+        actualVNode = (
+          <div>
+            <h1></h1>
+          </div>
+        )
+      })
+
+      then(() => {
+        expect(actualVNode).toStrictEqual(expectedVNode)
+      })
+    })
   })
 
-  it('Should return vnode with `ref` attribute', () => {
-    let actualVNode: any | vNode
-    let refName: any | string
-    let expectedVNode: any | vNode
+  describe('`render` function should create HTMLElement from vnode config', () => {
+    describe('created element should be equal element, which will be generated `h`', () => {
+      it('empty `div`', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
 
-    given(() => {
-      refName = 'test-ref'
-
-      expectedVNode = {
-        tag: 'div',
-        props: {
-          ref: refName,
-        },
-        child: [],
-      }
-    })
-
-    when(() => {
-      actualVNode = <div ref={refName}></div>
-    })
-
-    then(() => {
-      expect(actualVNode).toStrictEqual(expectedVNode)
-    })
-  })
-
-  it('Should return vnode with empty `attr`', () => {
-    let actualVNode: any | vNode
-    let expectedVNode: any | vNode
-
-    given(() => {
-      expectedVNode = {
-        tag: 'div',
-        props: null,
-        child: []
-      }
-    })
-
-    when(() => {
-      actualVNode = <div></div>
-    })
-
-    then(() => {
-      expect(actualVNode).toStrictEqual(expectedVNode)
-    })
-  })
-
-  it('Should return vnode with child', () => {
-    let actualVNode: any | vNode
-    let expectedVNode: any | vNode
-
-    given(() => {
-      expectedVNode = {
-        tag: 'div',
-        props: null,
-        child: [
-          {
-            tag: 'h1',
+        given(() => {
+          vNode = {
+            tag: 'div',
             props: null,
+            child: [],
+          }
+        })
+
+        given('Create div HTMLElement', () => {
+          $expectedNode = Core.render(<div />)
+        })
+
+        when(() => {
+          $actualNode = Core.render(vNode)
+        })
+
+        then(() => {
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
+
+      it('`div` with props', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
+
+        given(() => {
+          vNode = {
+            tag: 'div',
+            props: {
+              id: 'test-id',
+            },
+            child: [],
+          }
+        })
+
+        given('Create div HTMLElement', () => {
+          $expectedNode = Core.render(<div id='test-id'></div>)
+        })
+
+        when(() => {
+          $actualNode = Core.render(vNode)
+        })
+
+        then(() => {
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
+
+      it('`div` with multiple class', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
+
+        given(() => {
+          vNode = {
+            tag: 'div',
+            props: {
+              className: 'multiple class',
+            },
+            child: [],
+          }
+        })
+
+        given('Create div HTMLElement', () => {
+          $expectedNode = Core.render(<div className='multiple class'></div>)
+        })
+
+        when(() => {
+          $actualNode = Core.render(vNode)
+        })
+
+        then(() => {
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
+
+      it('`div` with nested `h` child', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
+
+        given(() => {
+          vNode = {
+            tag: 'div',
+            props: null,
+            child: [
+              {
+                tag: 'h1',
+                props: null,
+                child: [],
+              },
+            ],
+          }
+        })
+
+        given('Create div HTMLElement with child', () => {
+          $expectedNode = Core.render(
+            <div>
+              <h1></h1>
+            </div>
+          )
+        })
+
+        when(() => {
+          $actualNode = Core.render(vNode)
+        })
+
+        then(() => {
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
+
+      it('`div` with nested `h1` child with props', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
+
+        given(() => {
+          vNode = {
+            tag: 'div',
+            props: {
+              id: 'div',
+              className: 'multiple class',
+            },
+            child: [
+              {
+                tag: 'h1',
+                props: {
+                  id: 'h1',
+                  className: 'h1 class-name',
+                },
+                child: ['Header'],
+              },
+            ],
+          }
+        })
+
+        given('`div` with nested `h1`', () => {
+          $expectedNode = Core.render(
+            <div id='div' className='multiple class'>
+              <h1 id='h1' className='h1 class-name'>
+                Header
+              </h1>
+            </div>
+          )
+        })
+
+        when(() => {
+          $actualNode = Core.render(vNode)
+        })
+
+        then(() => {
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
+
+      it('`div` with ref attribute', () => {
+        let vNode: vNode
+        let $actualNode: HTMLElement
+        let $expectedNode: HTMLElement
+        let context: any
+
+        given(() => {
+          vNode = {
+            tag: 'div',
+            props: {
+              ref: 'div'
+            },
             child: []
           }
-        ]
-      }
-    })
 
-    when(() => {
-      actualVNode = (
-        <div>
-          <h1></h1>
-        </div>
-      )
-    })
+          context = {
+            refs: {}
+          }
+        })
 
-    then(() => {
-      expect(actualVNode).toStrictEqual(expectedVNode)
+        when(() => {
+          $actualNode = Core.render(vNode, context)
+        })
+
+        then(() => {
+          $expectedNode = context.refs.div
+          expect($actualNode.isEqualNode($expectedNode)).toBeTruthy()
+        })
+      })
     })
   })
 })
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -5,3 +5,55 @@ export function h (tag: string, props: any | null, ...child: string[] | vNode[])
     child
   }
 }
+
+export function render (vnode: vNode, context?: any): HTMLElement {
+  const { tag, props, child } = vnode
+  const $el = document.createElement(tag)
+  propsHandler($el, props, context)
+  childHandler($el, child, context)
+  return $el
+}
+
+function propsHandler ($el: HTMLElement, props: any, context?: any): void {
+  for (const prop in props) {
+    const value: any = props[prop]
+
+    // TODO: Research alternative realization
+    if (refPropHandler($el, prop, value, context)) continue
+    if (classPropHandler($el, prop, value)) continue
+    attributePropHandler($el, prop, value)
+  }
+}
+
+function refPropHandler ($el: HTMLElement, attr: string, value: string, context?: any): boolean {
+  if (attr === 'ref') {
+    context.refs[value] = $el
+    return true
+  }
+  return false
+}
+
+function classPropHandler ($el: HTMLElement, attr: string, value: any): boolean {
+  if (attr === 'className') {
+    value.split(' ').forEach((className: string) => {
+      $el.classList.add(className)
+    })
+    return true
+  }
+  return false
+}
+
+function attributePropHandler ($el: HTMLElement, attr: string, value: any): boolean {
+  $el.setAttribute(attr, value)
+  return true
+}
+
+function childHandler ($el: HTMLElement, child: string[] | vNode[], context?: any): void {
+  child.forEach((child: string | vNode) => {
+    if (typeof child === 'string') {
+      $el.append(child)
+    } else {
+      $el.append(render(child, context))
+    }
+  })
+}
