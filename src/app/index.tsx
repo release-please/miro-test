a66abbb64059a21d37f70390b1e7d2ccb41113b3
Dmitriy <qxwwxp@gmail.com>
2020-04-11 19:11:07 +0500

    perf(index): Reorganize code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { h } from './core'
 import * as Core from './core'
 import { Observer, isEmailValid } from './utils'
 
-interface AppItem {
+interface Item {
   id: string
   $el: HTMLElement
   [key: string]: any
@@ -18,30 +18,75 @@ interface InputEvent extends KeyboardEvent {
   target: InputElementTarget
 }
 
-class API {
-  refs: any = {}
-  items: Map<string, AppItem> = new Map()
-  $bus: Observer = new Observer()
+class EmailsInput {
+  rootEl!: HTMLElement
+  refs: any
+  items!: Map<string, Item>
+  $bus!: Observer
 
-  constructor (private readonly rootEl: HTMLElement) {
+  constructor (rootEl: HTMLElement) {
+    if (!(this instanceof EmailsInput)) {
+      return new EmailsInput(rootEl)
+    }
+
     this.rootEl = rootEl
+    this.refs = {}
+    this.items = new Map()
+    this.$bus = new Observer()
     this.render()
-    this.inputFocus()
+    this.focusInput()
   }
 
-  public addEmail (email: string): void {
-    if (typeof email !== 'string') return
+  private render (): void {
+    const $container = Core.render(
+      <div className='container'>
+        <div className='form'>
+          <div className='form__body'>
+            <h1 className='form__title'>
+              Shared <span className='form__title form__title_bold'>Board name</span> with others
+            </h1>
+            <main ref="board" className='board'>
+              {/* EMAILS WILL BE INSERTED HERE */ }
+              <input
+                ref="input"
+                type='text'
+                className='input'
+                placeholder='add more people…'
+                onblur={ (event: InputEvent) => { this.blurEventHandler(event) } }
+                onkeydown={ (event: InputEvent) => { this.keyDownEventHandler(event) } }
+              />
+            </main>
+          </div>
+          <div className='form__footer'>
+            <button className='btn btn_primary'>Add email</button>
+            <button className='btn btn_primary'>Get emails count</button>
+          </div>
+        </div>
+      </div>,
+      this
+    )
 
-    const trimmedEmail = email.trim()
+    this.rootEl.append($container)
+  }
 
+  public addEmail (value: string): void {
+    const trimmedEmail = value.trim()
     const isEmpty = (trimmedEmail === '')
     if (isEmpty) {
       this.cleanInput()
       return
     }
 
+    const email = this.prepareEmail(trimmedEmail)
+    this.insertEmail(email)
+
+    this.cleanInput()
+    this.focusInput()
+  }
+
+  private prepareEmail (value: string): Item {
     const ID = `EMAIL_${Math.random()}`
-    const isValid = isEmailValid(trimmedEmail)
+    const isValid = isEmailValid(value)
     const classValidOrNot = (isValid)
       ? 'email_valid'
       : 'email_invalid'
@@ -49,7 +94,7 @@ class API {
     const $email = Core.render(
       <div className={ `email ${classValidOrNot}` }>
         <div className='email__wrapper'>
-          <span className='email__text'>{ trimmedEmail }</span>
+          <span className='email__text'>{ value }</span>
           <button className='email__close'
             onclick={ () => { this.delEmail(ID) } }
           ></button>
@@ -57,25 +102,22 @@ class API {
       </div>
     )
 
-    const { board, input } = this.refs
-    board.insertBefore($email, input)
-
-    this.cleanInput()
-
     const result = {
       id: ID,
       $el: $email,
-      value: trimmedEmail,
-      valid: isEmailValid(trimmedEmail)
+      value: value,
+      isValid: isValid
     }
 
-    this.items.set(ID, result)
-
-    this.inputFocus()
+    return result
   }
 
-  private cleanInput (): void {
-    this.refs.input.value = ''
+  private insertEmail (email: Item): void {
+    const { board, input } = this.refs
+    const { id, $el: $email } = email
+
+    board.insertBefore($email, input)
+    this.items.set(id, email)
   }
 
   private delEmail (id: string): void {
@@ -105,49 +147,15 @@ class API {
     }
   }
 
-  private render (): void {
-    const $container = Core.render(
-      <div className='container'>
-        <div className='form'>
-          <div className='form__body'>
-            <h1 className='form__title'>
-              Shared <span className='form__title form__title_bold'>Board name</span> with others
-            </h1>
-            <main ref="board" className='board'>
-              {/* EMAILS */}
-              <input
-                ref="input"
-                type='text'
-                className='input'
-                placeholder='add more people…'
-                onblur={ (event: InputEvent) => { this.blurEventHandler(event) } }
-                onkeydown={ (event: InputEvent) => { this.keyDownEventHandler(event) }}
-              />
-            </main>
-          </div>
-          <div className='form__footer'>
-            <button className='btn btn_primary'>Add email</button>
-            <button className='btn btn_primary'>Get emails count</button>
-          </div>
-        </div>
-      </div>,
-      this
-    )
-
-    this.items.set('api', {
-      id: 'api',
-      $el: $container,
-      instance: this
-    })
-
-    this.rootEl.append($container)
+  private focusInput (): void {
+    this.refs.input.focus()
   }
 
-  private inputFocus (): void {
-    this.refs.input.focus()
+  private cleanInput (): void {
+    this.refs.input.value = ''
   }
 }
 
 export {
-  API
+  EmailsInput
 }
