1729719ff489f9ea0f1f50837bea6b0fea5d65da
Dmitriy <qxwwxp@gmail.com>
2020-04-11 23:48:27 +0500

    feat(styles): Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,18 @@ const config = {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
