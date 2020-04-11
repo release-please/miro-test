fc3d1192b8948b5badf9d9a1ae4babf52b072245
Dmitriy <qxwwxp@gmail.com>
2020-04-11 15:26:23 +0500

    refactor(layout): Implement board as reusable
    label >>> email

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,54 +6,52 @@ import * as Core from './core'
 document.body.onload = () => {
   const $app = Core.render(
     <div className='container'>
-      <div className='board'>
-        <div className='board__body'>
-          <h1 className='board__title'>
-            Shared <span className='board__title board__title_bold'>Board name</span> with others
+      <div className='form'>
+        <div className='form__body'>
+          <h1 className='form__title'>
+            Shared <span className='form__title form__title_bold'>Board name</span> with others
           </h1>
-          <main className='board__space'>
-            <div className='label label_valid'>
-              <div className='label__wrapper'>
-                <span className='label__text'>john@miro.com</span>
-                <button className='label__close'></button>
+          <main className='board'>
+            <div className='email email_valid'>
+              <div className='email__wrapper'>
+                <span className='email__text'>john@miro.com</span>
+                <button className='email__close'></button>
               </div>
             </div>
-            <div className='label label_invalid'>
-              <div className='label__wrapper'>
-                <span className='label__text'>invalid.email</span>
-                <button className='label__close'></button>
+            <div className='email email_invalid'>
+              <div className='email__wrapper'>
+                <span className='email__text'>invalid.email</span>
+                <button className='email__close'></button>
               </div>
             </div>
-            <div className='label label_valid'>
-              <div className='label__wrapper'>
-                <span className='label__text'>mike@miro.com</span>
-                <button className='label__close'></button>
+            <div className='email email_valid'>
+              <div className='email__wrapper'>
+                <span className='email__text'>mike@miro.com</span>
+                <button className='email__close'></button>
               </div>
             </div>
-            <div className='label label_valid'>
-              <div className='label__wrapper'>
-                <span className='label__text'>alexander@miro.com</span>
-                <button className='label__close'></button>
+            <div className='email email_valid'>
+              <div className='email__wrapper'>
+                <span className='email__text'>alexander@miro.com</span>
+                <button className='email__close'></button>
               </div>
             </div>
-            <div className='label label_valid'>
-              <div className='label__wrapper'>
-                <span className='label__text'>
+            <div className='email email_valid'>
+              <div className='email__wrapper'>
+                <span className='email__text'>
                   long non-breaking text long non-breaking text long non-breaking text
                 </span>
-                <button className='label__close'></button>
+                <button className='email__close'></button>
               </div>
             </div>
-            {/* HACK: Implement auto width */}
             <input
               type='text'
-              onkeydown="this.style.width = ((this.value.length + 1) * 8) + 'px';"
               className='input'
               placeholder='add more people…'
             />
           </main>
         </div>
-        <div className='board__footer'>
+        <div className='form__footer'>
           <button className='btn btn_primary'>Add email</button>
           <button className='btn btn_primary'>Get emails count</button>
         </div>
